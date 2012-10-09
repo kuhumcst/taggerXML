666f131ddb5f983b038d0c5df8eb4abef4c28d10
Bart Jongejan <bart.jongejan@gmail.com>
2012-10-09 16:38:14 +0200

    moved git remote instructions

--- doc/maketaggerXML.bash
+++ doc/maketaggerXML.bash
@@ -5,10 +5,10 @@ if [ ! -d hashmap ]; then
     mkdir hashmap
     cd hashmap
     git init
+    git remote add origin $METH://github.com/kuhumcst/hashmap.git
     cd ..
 fi
 cd hashmap
-git remote add origin $METH://github.com/kuhumcst/hashmap.git
 git pull origin master
 cd ..
 
@@ -16,10 +16,10 @@ if [ ! -d letterfunc ]; then
     mkdir letterfunc
     cd letterfunc
     git init
+    git remote add origin $METH://github.com/kuhumcst/letterfunc.git
     cd ..
 fi
 cd letterfunc
-git remote add origin $METH://github.com/kuhumcst/letterfunc.git
 git pull origin master
 cd ..
 
@@ -27,10 +27,10 @@ if [ ! -d parsesgml ]; then
     mkdir parsesgml
     cd parsesgml
     git init
+    git remote add origin $METH://github.com/kuhumcst/parsesgml.git
     cd ..
 fi
 cd parsesgml
-git remote add origin $METH://github.com/kuhumcst/parsesgml.git
 git pull origin master
 cd ..
 
@@ -38,10 +38,10 @@ if [ ! -d taggerXML ]; then
     mkdir taggerXML
     cd taggerXML
     git init
+    git remote add origin $METH://github.com/kuhumcst/taggerXML.git
     cd ..
 fi
 cd taggerXML
-git remote add origin $METH://github.com/kuhumcst/taggerXML.git
 git pull origin master
 cd src
 make all
